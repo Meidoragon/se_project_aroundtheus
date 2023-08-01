aa8f00b728f9c6ce6be16786e104aaf5f0946297
Meidoragon <104027002+Meidoragon@users.noreply.github.com>
2023-08-01 14:08:34 -0500

    add validation to first input of first form

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,22 +1,45 @@
 const options = {
   formSelector: ".modal__form",
   inputSelector: ".modal__input",
-  submitButtonSelector: ".modal__button", //modal__submit-button
-  inactiveButtonClass: "modal__button_disabled", //modal__submit-button_disabled
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "modal__button_disabled",
   inputErrorClass: "modal__input_type_error",
   errorClass: "modal__error_visible"
 }
 
-// const formElement = DOM.querySelector('.modal__form-container');
-// const formInput = formElement.querySelector('.form__input');
-// const formError = formElement.querySelector(`.${formInput.id}-error`);
+const formElement = DOM.querySelector(options.formSelector);
+const formInput = formElement.querySelector(options.inputSelector);
+const formError = formElement.querySelector(`.${formInput.id}-error`);
 
+const showError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add(options.inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(options.errorClass)
+};
+
+const hideError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove(options.inputErrorClass);
+  errorElement.classList.remove(options.errorClass);
+  errorElement.textContent = '';
+};
+
+const checkInputValidity = () => {
+  if (formInput.validity.valid) {
+    hideError(formElement, formInput);
+  } else {
+    showError(formElement, formInput, formInput.validationMessage);
+  }
+};
+
+formInput.addEventListener("input", (evt) => {
+  checkInputValidity();
+});
 
 
 const enableValidation = (options) => {
   
-}
-
-// DOM.addEventListener("keydown", (evt) => console.log(evt.key));
+};
 
 enableValidation(options); 
